889cbc5af12eceef36d7caab8b729d38d924680c
kabertin <karindabertin35@gmail.com>
2023-12-04 10:35:57 -0800

    again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i != 'c' and 'C':
+        if i != "c" and i != "C":
             new_str = new_str + i
     return new_str
