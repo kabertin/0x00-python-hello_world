a7b3f3d1756d289031fadc953b17cee3a0398f80
kabertin <karindabertin35@gmail.com>
2023-12-05 06:58:11 -0800

    the greater number8

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    else:
-        comp = my_list[0]
-        from num in my_list:
-            if num >= comp:
-                comp = num
-    return comp
+    new_list = []
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
